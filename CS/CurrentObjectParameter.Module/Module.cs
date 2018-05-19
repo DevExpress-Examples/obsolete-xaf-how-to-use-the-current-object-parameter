eb51928056eae8683bf4637092b4a4ae9e30ce55
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/CurrentObjectParameter.Module/Module.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace CurrentObjectParameter.Module {
+    public sealed partial class CurrentObjectParameterModule : ModuleBase {
+        public CurrentObjectParameterModule() {
+            InitializeComponent();
+        }
+    }
+}
