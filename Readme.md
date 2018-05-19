396c39583ede59669b2d1e69ccace56e7a752cec
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# OBSOLETE - How to Use the Current Object Parameter (v9.2.4 - v13.1.12)
+
+
+<p>This example is now obsolete. See the up-to-date description in the <a href="http://documentation.devexpress.com/#Xaf/CustomDocument3204">Current Object Parameter</a> help topic.</p>
+
+<br/>
+
+
---
+++ VB/CurrentObjectParameterVB/CurrentObjectParameter.Module.Web/WebModule.vb
@@ -0,0 +1,15 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+
+Imports DevExpress.ExpressApp
+
+Namespace CurrentObjectParameter.Module.Web
+    <ToolboxItemFilter("Xaf.Platform.Web")> _
+    Public NotInheritable Partial Class CurrentObjectParameterAspNetModule
+        Inherits ModuleBase
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
