98ac077ae02edbf893c17b3081845e1d7126d3a9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/CurrentObjectParameter.Module.Web/WebModule.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace CurrentObjectParameter.Module.Web {
+    [ToolboxItemFilter("Xaf.Platform.Web")]
+    public sealed partial class CurrentObjectParameterAspNetModule : ModuleBase {
+        public CurrentObjectParameterAspNetModule() {
+            InitializeComponent();
+        }
+    }
+}
